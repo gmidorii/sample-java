7206ca9d72cc4c113e7661e24f46ba334ed0cc46
tagasouichiro <stitow14@gmail.com>
2017-02-16 22:03:18 +0900

    remove unnecessary imports

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,3 @@
-import com.google.common.collect.Lists;
-
-import java.util.EnumSet;
-import java.util.List;
-import java.util.Optional;
-import java.util.Set;
-import java.util.function.*;
-import java.util.stream.Collector;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
 /**
  * Created by midori on 2017/02/15.
  */
