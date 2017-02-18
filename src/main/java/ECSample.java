c3a988ddb82136734cc32ff8eef8f8473c60ac7b
tagasouichiro <stitow14@gmail.com>
2017-02-18 09:25:37 +0900

    add reject

--- src/main/java/ECSample.java
+++ src/main/java/ECSample.java
@@ -1,7 +1,10 @@
+import org.eclipse.collections.api.list.ImmutableList;
 import org.eclipse.collections.impl.factory.Lists;
 import org.eclipse.collections.impl.list.mutable.FastList;
 
 import java.util.List;
+
+import static org.eclipse.collections.impl.factory.Lists.immutable;
 
 /**
  * Created by midori on 2017/02/16.
@@ -13,10 +16,11 @@ public class ECSample {
 		/**
 		 * select(Predicate<? super T> predicate)
 		 */
-		Lists.immutable
+		immutable
 				.of(1, 2, 3, 4, 5)
 				.select(x -> x > 3)
 				.forEach(System.out::print);
+		System.out.println();
 
 		// ============================
 		// List
@@ -24,6 +28,15 @@ public class ECSample {
 		List<Integer> l1 = FastList.newList();
 		List<Integer> l2 = FastList.newListWith(1, 2, 3, 4);
 		List<Integer> l3 = Lists.mutable.of(1, 2, 3);
+		// Type must be ImmutableList<>
+		ImmutableList<Integer> l4 = Lists.immutable.of();
 
+		/**
+		 * reject(Predicate<? super T> predicate)
+		 */
+		immutable
+				.of(1, 2, 3, 4, 5)
+				.reject(x -> x > 3)
+				.forEach(System.out::print); //123
 	}
 }
