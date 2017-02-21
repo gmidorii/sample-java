a95c0aaaca74420bb116ebc32e5df1865bacd572
tagasouichiro <stitow14@gmail.com>
2017-02-21 21:32:01 +0900

    add block

--- src/main/java/ECSample.java
+++ src/main/java/ECSample.java
@@ -36,54 +36,62 @@ public class ECSample {
 		/**
 		 * reject(Predicate<? super T> predicate)
 		 */
-		immutable
-				.of(1, 2, 3, 4, 5)
-				.reject(x -> x > 3)
-				.forEach(System.out::print); //123
-		newLine();
+		{
+			immutable
+					.of(1, 2, 3, 4, 5)
+					.reject(x -> x > 3)
+					.forEach(System.out::print); //123
+			newLine();
+		}
 
 		/**
 		 * partition(Predicate<? super T> predicate)
 		 */
-		PartitionImmutableList<Integer> partitionList =  immutable
-				.of(1, 2, 3, 4, 5)
-				.partition(x -> x > 3);
-		System.out.println(partitionList.getSelected()); // [4, 5]
-		System.out.println(partitionList.getRejected()); // [1, 2, 3]
+		{
+			PartitionImmutableList<Integer> partitionList = immutable
+					.of(1, 2, 3, 4, 5)
+					.partition(x -> x > 3);
+			System.out.println(partitionList.getSelected()); // [4, 5]
+			System.out.println(partitionList.getRejected()); // [1, 2, 3]
+		}
 
 		/**
 		 * collect(Function<? super T,? extends V> function)
 		 */
-		immutable
-				.of("one", "two", "three", "four", "five")
-				.collect(x -> x.length())
-				.forEach(System.out::print); //33544
-		newLine();
-		System.out.println(
-				createTrains()
-					.collect(t -> t.getName()) // [nozomi, hikari, yamanote, ginza]
-		);
-		createTrains()
-			.collect(t -> {
-				String name = t.getName();
-				return new Train(name);
-			})
-			.forEach(x -> {
-					System.out.print(x.getId());
-					System.out.print(" ,");
-			}); // nozomi ,hikari ,yamanote ,ginza ,
-		newLine();
+		{
+			immutable
+					.of("one", "two", "three", "four", "five")
+					.collect(x -> x.length())
+					.forEach(System.out::print); //33544
+			newLine();
+			System.out.println(
+					createTrains()
+							.collect(t -> t.getName()) // [nozomi, hikari, yamanote, ginza]
+			);
+			createTrains()
+					.collect(t -> {
+						String name = t.getName();
+						return new Train(name);
+					})
+					.forEach(x -> {
+						System.out.print(x.getId());
+						System.out.print(" ,");
+					}); // nozomi ,hikari ,yamanote ,ginza ,
+			newLine();
+		}
 
 		/**
 		 * flatCollect(Function<? super T,? extends Iterable<V>> function)
 		 * XXX jdk -> flatMap
 		 */
-		immutable
-				.of(createTrains(),
-						createTrains())
-				.flatCollect(ts -> ts.collect(t -> t.getId()))
-				.forEach(System.out::print); //12341234
-		newLine();
+		{
+			immutable
+					.of(createTrains(),
+							createTrains())
+					.flatCollect(ts -> ts.collect(t -> t.getId()))
+					.forEach(System.out::print); //12341234
+			newLine();
+		}
 
 		/**
 		 * groupBy(Function<? super T,? extends V> function)
